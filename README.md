48ff2a0d698bf7d9d7e196ed6c2f7cd27ac9791d
yansheng836 <sheng.yan836@gmail.com>
2025-10-02 00:08:11 +0800

    chore: backup files on 2025-10-02  0:08:11

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # figure-relationship-diagram
+
 人物关系图
 
+## GitHub Page HTML访问路径
+
+参考实例：
+
+index3.html：<https://yansheng836.github.io/figure-relationship-diagram/index3.html>
+
+---
+
+<!-- START_TOC_GENERATED -->
+index.html: <https://yansheng836.github.io/figure-relationship-diagram/index.html>
+index2.html: <https://yansheng836.github.io/figure-relationship-diagram/index2.html>
+index3.html: <https://yansheng836.github.io/figure-relationship-diagram/index3.html>
+index_fixed1-示例数据.html: <https://yansheng836.github.io/figure-relationship-diagram/index_fixed1-示例数据.html>
+index_fixed2-真实数据.html: <https://yansheng836.github.io/figure-relationship-diagram/index_fixed2-真实数据.html>
+index_fixed3-优化.html: <https://yansheng836.github.io/figure-relationship-diagram/index_fixed3-优化.html>
---
+++ gen_urls.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# 备份原文件
+cp README.md README.md.backup.$(date +%Y%m%d%H%M%S)
+
+# 生成 TOC 内容
+str=''
+for i in `ls *.html 2>/dev/null`
+do   
+    if [ -n "$str" ]; then
+        str="$str\n"
+    fi
+    str="$str$i: <https://yansheng836.github.io/figure-relationship-diagram/$i>"
+done
+
+# 定义标志模式
+start_pattern='<!-- START_TOC_GENERATED -->'
+end_pattern='<!-- END_TOC_GENERATED -->'
+
+# 使用 awk 进行精确替换（最可靠的方法）
+awk -v start="$start_pattern" \
+    -v end="$end_pattern" \
+    -v new_content="$str" '
+    BEGIN { output=1 }
+    $0 == start { 
+        print $0
+        print new_content
+        output=0
+        next
+    }
+    $0 == end && output == 0 {
+        print $0
+        output=1
+        next
+    }
+    output == 1 { print $0 }
+' README.md > README.md.tmp
+
+# 检查并替换
+if [ $? -eq 0 ] && [ -s README.md.tmp ]; then
+    mv README.md.tmp README.md
+    echo "TOC 更新成功！"
+else
+    echo "更新失败，已保留备份文件"
+    rm -f README.md.tmp
+fi
